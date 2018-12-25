08006768290f8d55dbec868f502ccd518beec8fd
ycjw <ycjw420@gmail.com>
2018-12-25 20:01:01 +0800

    初始化项目

---
+++ src/main/java/com/ycjw/technology/TechnologyApplication.java
@@ -0,0 +1,14 @@
+package com.ycjw.technology;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnologyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechnologyApplication.class, args);
+	}
+
+}
+
